9e776b54fc0fb7fbefb63418ac75b5252c19699a
beanstown106 <nbenis106@gmail.com>
2017-06-07 15:47:06 -0300

    vendor: use pixel theme for gboard if installed to system
    Change-Id: Ibb81d0afb57bf5fecca13dc51976596eeb4f1f73

--- configs/common.mk
+++ configs/common.mk
@@ -3,3 +3,6 @@ DEVICE_PACKAGE_OVERLAYS += vendor/overlay/aoscp/overlay/common
 
 # Enable Google Assistant
 PRODUCT_PROPERTY_OVERRIDES += ro.opa.eligible_device=true
+
+# Set Pixel blue light theme on Gboard
+PRODUCT_PROPERTY_OVERRIDES += ro.com.google.ime.theme_id=5
