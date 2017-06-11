0589ef21aa6aafaaf8f2efcdbebdc3de3943a6e4
Chris <chris@cypheros.co>
2017-06-11 17:45:04 -0400

    vendor: fix package directory path
    Change-Id: Icbb423154a0934fc0193f018d7d0223fc03ae0ec

--- configs/common.mk
+++ configs/common.mk
@@ -1,5 +1,5 @@
 # Common overlay
-DEVICE_PACKAGE_OVERLAYS += vendor/pixel/overlay/common
+DEVICE_PACKAGE_OVERLAYS += vendor/overlay/aoscp/overlay/common
 
 # Enable Google Assistant
 PRODUCT_PROPERTY_OVERRIDES += ro.opa.eligible_device=true
