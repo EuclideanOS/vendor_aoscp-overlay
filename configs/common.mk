dc37fc6890878b7327c84fc9b1606badf502722e
Marco Marinho <marcomarinho12@gmail.com>
2017-12-10 15:45:36 +0100

    vendor: remove useless overlays & set the right path to vendor
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>

--- configs/common.mk
+++ configs/common.mk
@@ -1,5 +1,5 @@
 # Common overlay
-DEVICE_PACKAGE_OVERLAYS += vendor/aoscp-overlay/overlay/common
+DEVICE_PACKAGE_OVERLAYS += vendor/pixel/overlay/common
 
 # Enable Google Assistant
 PRODUCT_PROPERTY_OVERRIDES += ro.opa.eligible_device=true
